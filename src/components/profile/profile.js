bb7fd878b8b2aff7a634ba615036a61784446082
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-01-10 10:02:02 +0100

    test

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -5,11 +5,11 @@ export default class Profile extends React.Component {
   render() {
     return (
         <div className="profile">
-            <h2>Jacob Juul Larsen</h2>
+            {/* <h2>Jacob Juul Larsen</h2>
                 <h4>PHP, HTML, javscript</h4>
                 <h5>lavarel, yii, wordpress</h5>
                 <h5>jquery, node.js, react</h5>
-                <h5>sass, webpack, grunt, gulp</h5>
+                <h5>sass, webpack, grunt, gulp</h5> */}
         </div>
     );
   }
