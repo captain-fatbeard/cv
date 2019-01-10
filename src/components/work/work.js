a15029a258ce84860b3dde4f35ccbd46fa1bce47
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-01-10 13:54:39 +0100

    work section added

---
+++ src/components/work/work.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./work.scss"
+
+export default class Work extends React.Component {
+    render() {
+        return (
+            <div className="work">
+                <div className="item">
+                    <div className="heading">
+                        <span className="circle"></span>
+                        2015 - 2019
+                    </div>
+                    <div className="body">
+                        <h2>Lorem</h2>
+                        <p>Lorem ipsum dolor sit amet, ad ullum veniam nominati sea. Dicat probatus concludaturque sea ne. Usu indoctum consetetur scriptorem at, porro mundi eam id. Has sensibus inciderint ex, commodo philosophia mediocritatem eam ex, nisl quando audire an eam.</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="heading">
+                        <span className="circle"></span>
+                        2013 - 2015
+                    </div>
+                    <div className="body">
+                        <h2>In feugait</h2>
+                        <p>In feugait recusabo vis, ex has tota omnes inciderint, debitis facilisi platonem ne nam. Expetenda principes id usu. At denique eligendi detraxit est, ad facilis qualisque suscipiantur sea. Viris feugiat ad duo, cum cu iudico option. Eruditi evertitur id his.</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="heading">
+                        <span className="circle"></span>
+                        2012
+                    </div>
+                    <div className="body">
+                        <h2>Odio</h2>
+                        <p>Odio aliquip vivendum nam ad, duo iisque indoctum ad, cu ornatus dolorum qui. Ad quo viris sapientem deseruisse, in paulo menandri nec. Periculis signiferumque mea ea, sonet definitiones qui eu. Ad malis paulo dolore sea. Adhuc comprehensam at cum. No maiorum tractatos conclusionemque duo, solum invidunt ut vim.</p>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="heading">
+                        <span className="circle"></span>
+                        2011
+                    </div>
+                    <div className="body">
+                        <h2>Ut pro fuisset</h2>
+                        <p>Ut pro fuisset singulis dissentiet, ei sadipscing scriptorem eum. Et mel utroque euripidis. Ut nec mandamus sapientem quaerendum, vim ne option assentior, etiam gloriatur at pri. Ei minim evertitur nam, at pri dicunt ornatus.</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
