c7f6453987341fd1e693aeabfa3d4a46e8789fe3
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-01-10 14:08:20 +0100

    tools

--- src/components/toolbox/toolbox.js
+++ src/components/toolbox/toolbox.js
@@ -2,11 +2,42 @@ import React from "react";
 import "./toolbox.scss"
 
 export default class Toolbox extends React.Component {
-  render() {
-    return (
-        <div className="toolbox">
-            toolbox
-        </div>
-    );
-  }
+	render() {
+		return (
+        	<div className="toolbox">
+            	<h2>toolbox</h2>
+
+				frontend
+					Bootstrap
+					ReactJS
+					Vue
+					JS
+					jquery
+				
+				backend
+					NodeJS
+					Laravel
+					Wordpress
+
+				Git
+
+				Developer Tools
+					sass
+					webpack
+					grunt & gulp
+
+				Package Managers
+					NPM
+					Composer
+					Chocolatey
+
+				Languages
+					PHP
+					HTML
+					javascript
+
+                
+        	</div>
+    	);
+	}
 }
