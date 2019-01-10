dd3aa1d7b47a6b4f557b052ff610cb156ff09086
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-01-10 13:55:06 +0100

    toolbox added

---
+++ src/components/toolbox/toolbox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./toolbox.scss"
+
+export default class Toolbox extends React.Component {
+  render() {
+    return (
+        <div className="toolbox">
+            toolbox
+        </div>
+    );
+  }
+}
