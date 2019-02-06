ee50308ed72cd1b0c77f6032edf0a6e3556a1aa2
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-02-06 20:10:14 +0100

    arays fun

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -25,34 +25,56 @@ export default class Graph extends React.Component {
             }
         };
         
-        // let skills = []
-        // skills.push({area:'language', label: 'PHP', val: 90});
-        // skills.push({area:'language', label: 'HTML', val: 99});
-        // skills.push({area:'language', label: 'css', val: 99});
-        // skills.push({area:'language', label: 'javascript', val: 80});
-        // skills.push({area:'language', label: 'mysql', val: 70});
-        // skills.push({area:'frontend', label: 'VueJS', val: 50});
-        // skills.push({area:'frontend', label: 'ReactJS', val: 55});
-        // skills.push({area:'frontend', label: 'Bootstrap', val: 95});
-        // skills.push({area:'frontend', label: 'jquery', val: 95});
+        let skills = [];
+        skills.push({area:'language', label: 'HTML', val: 99});
+        skills.push({area:'language', label: 'PHP', val: 90});
+        skills.push({area:'language', label: 'mysql', val: 70});
+        skills.push({area:'language', label: 'javascript', val: 80});
+        skills.push({area:'language', label: 'css', val: 99});
+        skills.push({area:'frontend', label: 'ReactJS', val: 55});
+        skills.push({area:'frontend', label: 'Bootstrap', val: 95});
+        skills.push({area:'frontend', label: 'VueJS', val: 50});
+        skills.push({area:'frontend', label: 'jquery', val: 95});
    
+        let labels = [];
+        let dataSetsLanguages = [];
+        let dataSetsFrontend = [];
 
-        let skillsGraph = document.getElementById('skills-graph');
+        for (let index = 0; index < skills.length; index++) {
+            labels.push(skills[index].label);
+
+            if (skills[index].area === 'language') {
+                dataSetsLanguages.push(skills[index].val);
+            } else {
+                dataSetsLanguages.push(0);
+            }
+            
+            if (skills[index].area === 'frontend') {
+                dataSetsFrontend.push(skills[index].val);
+            } else {
+                dataSetsFrontend.push(0);
+            }
+        }
+        console.log(labels);
+        console.log(dataSetsLanguages);
+        console.log(dataSetsFrontend);
+
+        const skillsGraph = document.getElementById('skills-graph');
         new Chart(skillsGraph, {
             type: 'radar',
             data: {
-                labels: ['PHP', 'HTML', 'css', 'javascript', 'mysql', 'VueJS', 'ReactJS', 'Bootstrap', 'jquery'],
+                labels: labels,
                 datasets: [
                     {
-                        label: 'Languages',
-                        data:[90, 99, 99, 70, 81, 0, 0, 0, 0],
+                        label: 'languages',
+                        data: dataSetsLanguages,
                         fill: true,
                         backgroundColor: 'rgba(255, 99, 132, 0.1)',
                         borderColor: 'rgb(255, 99, 132)',
                     },
                     {
-                        label: 'Frontend',
-                        data:[0, 0, 0, 0, 0, 60, 60, 95, 95],
+                        label: 'frontend',
+                        data: dataSetsFrontend,
                         fill: true,
                         backgroundColor: 'rgba(132, 99, 255, 0.1)',
                         borderColor: 'rgb(132, 99, 255)',
