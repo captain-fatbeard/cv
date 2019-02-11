ebe4abbf7f02d6e1af3ee2cb21b373777a425928
Jacob Larsen <Jacob.larsen@netbooster.com>
2019-02-11 13:19:14 +0100

    graph updates

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -3,12 +3,46 @@ import Chart from 'chart.js';
 import "./graph.scss"
 
 export default class Graph extends React.Component {
-    constructor() {
-        super();
-        this.state = { areasOfSkills: [] }
+    constructor(props) {
+        super(props);
+        this.state = {
+            areasOfSkills: [],
+            skills: {
+                'languages': {
+                    'backgroundColor': 'rgba(255, 99, 132, 0.1)',
+                    'borderColor': 'rgb(255, 99, 132)',
+                    'data': {
+                        'html': 99,
+                        'PHP': 90,
+                        'mysql': 70,
+                        'javascript': 80,
+                        'css': 99,
+                    }
+                },
+                'Backend': {
+                    'backgroundColor': 'rgba(255, 132, 99, 0.1)',
+                    'borderColor': 'rgb(255, 132, 99)',
+                    'data': {
+                        'NodeJS': 70,
+                        'Laravel': 90,
+                        'Wordpress': 90
+                    }
+                },
+                'frontend': {
+                    'backgroundColor': 'rgba(132, 99, 255, 0.1)',
+                    'borderColor': 'rgb(132, 99, 255)',
+                    'data': {
+                        'ReactJS': 55,
+                        'Bootstrap': 95,
+                        'VueJS': 50,
+                        'jquery': 95
+                    }
+                }
+            }
+        }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let grapOptions = {
             elements: {
                 line: {
@@ -30,53 +64,19 @@ export default class Graph extends React.Component {
             }
         };
         
-        const skills = {
-            'languages': {
-                'backgroundColor': 'rgba(255, 99, 132, 0.1)',
-                'borderColor': 'rgb(255, 99, 132)',
-                'data': {
-                    'html': 99,
-                    'PHP': 90,
-                    'mysql': 70,
-                    'javascript': 80,
-                    'css': 99,
-                }
-            },
-            'Backend': {
-                'backgroundColor': 'rgba(255, 132, 99, 0.1)',
-                'borderColor': 'rgb(255, 132, 99)',
-                'data': {
-                    'NodeJS': 70,
-                    'Laravel': 90,
-                    'Wordpress': 90
-                }
-            },
-            'frontend': {
-                'backgroundColor': 'rgba(132, 99, 255, 0.1)',
-                'borderColor': 'rgb(132, 99, 255)',
-                'data': {
-                    'ReactJS': 55,
-                    'Bootstrap': 95,
-                    'VueJS': 50,
-                    'jquery': 95
-                }
-            }
-        };
-
         let size = 0;
-        for (let [label, dataset] of Object.entries(skills)) {
+        for (let [label, dataset] of Object.entries(this.state.skills)) {
             for (let [skill, value] of Object.entries(dataset.data)) {
                 size++;
             }
         }
 
-        let areas = [];
         let labels = [];
         let datasets = [];
         let dataindex = 0;
-        for (let [label, dataset] of Object.entries(skills)) {
+        for (let [label, dataset] of Object.entries(this.state.skills)) {
             let data = [];
-            areas.push(label);
+            this.state.areasOfSkills.push(label);
             for (let index = 0; index < size; index++) {
                 data.push(0);
             }
@@ -97,28 +97,34 @@ export default class Graph extends React.Component {
                 data: data
             });
         }
-        
-        // this.setState({ areas: areasOfSkill } ); 
-        // console.log(labels);
-        // console.log(datasets);
-        // console.log(areas);
 
-        // const skillsGraph = document.getElementById('skills-graph');
+        this.labels = labels;
+        this.datasets = datasets;
+        this.grapOptions = grapOptions;
+
+    }
+
+    componentDidMount() {
+        const skillsGraph = document.getElementById('skills-graph');
         // new Chart(skillsGraph, {
         //     type: 'radar',
         //     data: {
-        //         labels: labels,
-        //         datasets: datasets
+        //         labels: this.labels,
+        //         datasets: this.datasets
         //     },
-        //     options:grapOptions
+        //     options: this.grapOptions
         // });
     }
 
+
     render() {
-        console.log('areas');
+        // const areas = this.state.areasOfSkills.map((area, key) => 
+        //     <a key={key} href="">{area}</a>
+        // );
+
         return (
-            <div className="graph">
-                {/* {areas.map(area => <a href="" className=""> {area} </a>)} */}
+            <div className="graph">   
+                {/* {this.state.areasOfSkills.map((area, key) => <a className="graph-link" key={key} href="#">{area}</a>)} */}
                 <canvas id="skills-graph" width="400" height="400"></canvas>
             </div>
         );
