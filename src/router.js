7f7be9c0e76b4b9c4b166df69e7db1f55f00757a
Jacob Larsen <jacob.larsen@netbooser.com>
2020-07-01 18:26:23 +0200

    test

--- src/router.js
+++ src/router.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 
-import VueScrollTo from 'vue-scrollto';
 import VueScrollactive from 'vue-scrollactive';
-
+Vue.use(VueScrollactive);
 // You can also pass in the default options
+import VueScrollTo from 'vue-scrollto';
 Vue.use(VueScrollTo, {
     container: 'body',
     duration: 500,
@@ -19,17 +19,13 @@ Vue.use(VueScrollTo, {
     x: false,
     y: true
 })
-Vue.use(VueScrollactive);
-
 
 Vue.use(Router)
-
 export default new Router({
     mode: 'history',
     routes: [{
         path: '/',
         name: 'home',
-        component: Home,
-        base: '/cv/'
+        component: Home
     }]
 })
