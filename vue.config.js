b4e9cc09e066616a4f9656e986605aaaedf7789a
Jacob Larsen <jacob.larsen@netbooser.com>
2020-07-01 16:51:25 +0200

    new build

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,17 @@ module.exports = {
                 `
             }
         }
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: {
+                minSize: 10000,
+                maxSize: 250000,
+            }
+        }
     }
 }
+
+
+
+
